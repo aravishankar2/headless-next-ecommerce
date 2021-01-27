f18a9a77bcd96e397be3bc987166d2969d33556e
Arun Ravishankar <aravishankar2@gmail.com>
2021-01-26 19:00:59 -0600

    cloudinary images not loading..

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    images: {
-      loader: "cloudinary",
-      path: "http://res.cloudinary.com/surface-group",
-      domains: ["res.cloudinary.com"]
-    },
-    publicRuntimeConfig: {
-      CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-      CONTENTFUL_ENVIRONMENT: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT,
-      CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    },
-  };
+  images: {
+    loader: "cloudinary",
+    path: "http://res.cloudinary.com/surface-group",
+    domains: ["res.cloudinary.com"],
+  },
+  publicRuntimeConfig: {
+    CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_ENVIRONMENT: process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT,
+    CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    PUBLIC_SNIPCART_API_KEY: process.env.NEXT_PUBLIC_SNIPCART_API_KEY,
+  },
+};
