ed2d408da303e4f2d7466566edcdf2ee286f4b1d
Arun Ravishankar <aravishankar2@gmail.com>
2021-01-28 12:34:57 -0600

    most basic implementation of netlify auth implemented, I want to implement redux so the users state will be accessible throughout the project

--- components/Nav.component.jsx
+++ components/Nav.component.jsx
@@ -1,27 +1,58 @@
+import { useState, useEffect } from "react";
+import netlifyAuth from "../netlifyAuth";
 import { Navbar } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
-const Nav = () => (
-  <>
-    <Navbar bg="light" expand="sm">
-      <div className="container d-flex justify-content-between align-items-center">
-        <Navbar.Brand href="/shop">E - Commerce</Navbar.Brand>
-        <div>
-          <a
-            className="snipcart-checkout snipcart-summary"
-            // href="#"
-            // style={{ textDecoration: "none" }}
-          >
-            <div className="d-flex justify-content-between align-items-center">
-            <small className="snipcart-items-count text-muted "></small>
-            <span className="mr-2"><FaShoppingCart /></span>{" "}
-            <small className="snipcart-total-price">$0.00</small>
+const Nav = () => {
+  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+  let [user, setUser] = useState(null);
+  let login = () => {
+    netlifyAuth.authenticate((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+      netlifyAuth.closeModal();
+    });
+  };
 
-            </div>
-          </a>
+  let logout = () => {
+    netlifyAuth.signout(() => {
+      setLoggedIn(!!user) + setUser(user);
+    });
+  };
+  useEffect(() => {
+    netlifyAuth.initialize((user) => {
+      setLoggedIn(!!user);
+      setUser(user);
+    });
+  }, [loggedIn]);
+  return (
+    <>
+      <Navbar bg="light" expand="sm">
+        <div className="container d-flex justify-content-between align-items-center">
+          <Navbar.Brand href="/shop">E - Commerce</Navbar.Brand>
+          {loggedIn ? (
+            <div onClick={logout}>You are logged in!</div>
+          ) : (
+            <button onClick={login}>Log in here.</button>
+          )}
+          <div>
+            <a
+              className="snipcart-checkout snipcart-summary"
+              // href="#"
+              // style={{ textDecoration: "none" }}
+            >
+              <div className="d-flex justify-content-between align-items-center">
+                {/* <small className="snipcart-items-count text-muted "></small> */}
+                <span className="mr-2">
+                  <FaShoppingCart />
+                </span>{" "}
+                <small className="snipcart-total-price">$0.00</small>
+              </div>
+            </a>
+          </div>
         </div>
-      </div>
-    </Navbar>
-  </>
-);
+      </Navbar>
+    </>
+  );
+};
 
 export default Nav;
